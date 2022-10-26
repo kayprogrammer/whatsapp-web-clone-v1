74c9cd690bbd38f15ee211a564b6e8f85c5c9e5d
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-10-26 19:48:54 +0100

    Project Setup (DB, Settings, Apps etc)

---
+++ apps/common/middlewares.py
@@ -0,0 +1,19 @@
+from django.utils import timezone
+
+class TimezoneMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        user = request.user
+
+        try:
+            tzname = user.timezone.name if user.timezone else None
+            timezone.activate(tzname)
+        except:
+            timezone.deactivate()
+        
+        response = self.get_response(request)
+        return response
+        
+        
