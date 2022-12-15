7779cf49e308ec493ff28b631c4b6165c5780640
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-12-15 23:49:24 +0100

    modified views

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -16,11 +16,21 @@ User = get_user_model()
 class HomeView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         # request.session.flush()
+
         user = request.user
-        messages = Message.objects.filter(Q(sender=user) | Q(receiver=user)).select_related('sender', 'receiver')
-        inbox_list = messages.annotate(other=Case(When(sender=user, then=F('receiver')), default=F('sender'), output_field=CharField())).order_by('other', '-created_at').distinct('other')
+        
+        messages = Message.objects.filter(
+            Q(sender=user) | Q(receiver=user)
+        ).select_related('sender', 'receiver')
+        
+        inbox_list = messages.annotate(
+            other=Case(When(sender=user, then=F('receiver')), default=F('sender'), output_field=CharField())
+        ).order_by('other', '-created_at').distinct('other')
         sorted_inbox_list = sorted(inbox_list, key=lambda x: x.created_at, reverse=True)
+        
         all_users = User.objects.filter(is_email_verified=True, is_phone_verified=True, is_active=True).exclude(id=user.id).order_by('name')
+
+        print(sorted_inbox_list)
         return render(request, 'chat/index.html', context={'inbox_list': sorted_inbox_list, 'messages': messages, 'all_users': all_users})
 
 class ShowDirectMessagesView(LoginRequiredMixin, View):
